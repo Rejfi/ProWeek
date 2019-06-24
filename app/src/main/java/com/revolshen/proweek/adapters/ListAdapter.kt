447083a877b1fd63ff0f417ceb1292fa28905032
Rejfi <rrafaul@wp.pl>
2019-06-24 18:18:23 +0200

    Add clear all details in editTaskFragment and add onClick hide menu

--- app/src/main/java/com/revolshen/proweek/adapters/ListAdapter.kt
+++ app/src/main/java/com/revolshen/proweek/adapters/ListAdapter.kt
@@ -3,6 +3,7 @@ package com.revolshen.proweek.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.ListAdapter
@@ -11,6 +12,8 @@ import com.revolshen.proweek.R
 import com.revolshen.proweek.data.Task
 
 class RecyclerAdapter : ListAdapter<Task, RecyclerAdapter.TaskViewHolder>(DIFF_CALLBACK) {
+
+    private lateinit var onClickItemListener: AdapterView.OnItemClickListener
 
     companion object{
         val DIFF_CALLBACK = object : ItemCallback<Task>(){
@@ -34,20 +37,27 @@ class RecyclerAdapter : ListAdapter<Task, RecyclerAdapter.TaskViewHolder>(DIFF_C
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
 
         val task = getItem(holder.adapterPosition)
-
         holder.taskText.text = task.text
         holder.taskPriority.text = task.priority.toString()
         holder.taskDate.setText("Data")
+
+
+        TODO("Zaimplementować onClick na poszczególnej notatce tak aby przenosiła wszystkie informacje do editTaskFragment")
     }
 
+
     inner class TaskViewHolder(view: View): RecyclerView.ViewHolder(view){
+
 
         val taskText: TextView = view.findViewById(R.id.taskText)!!
         val taskCheckBox: TextView = view.findViewById(R.id.taskCheckBox)!!
         val taskDate: TextView = view.findViewById(R.id.taskFinishDate)!!
         val taskPriority: TextView = view.findViewById(R.id.taskPriority)!!
 
+
+
     }
+
 
 }
 
---
+++ app/src/main/java/com/revolshen/proweek/fragments/EditTaskFragment.kt
@@ -0,0 +1,56 @@
+package com.revolshen.proweek.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.revolshen.proweek.R
+import com.revolshen.proweek.data.Task
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.edit_task_fragment.*
+import kotlinx.android.synthetic.main.my_tasks_fragment.*
+
+class EditTaskFragment : Fragment(){
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.edit_task_fragment, container, false)
+
+        TODO("Przygotować do końca i przemyśleć layout editTaskFragment aby był jak najbardziej przyjazny dla uzytkownika")
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setNewTask.setOnClickListener {
+            val task: Task = Task(
+                editTitleTask.text.toString(),
+                editDescriptionTask.text.toString(),
+                0,
+                0,
+                editRatingBar.rating.toInt()
+            )
+
+            MyTaskFragment.taskViewModel.insert(task)
+            activity?.viewPager?.currentItem = 0
+
+            editTitleTask.setText("")
+            editDescriptionTask.setText("")
+            editRatingBar.rating = 0.0f
+            editFloatMenu.close(true)
+        }
+
+        clearAllDetails.setOnClickListener {
+            editTitleTask.setText("")
+            editDescriptionTask.setText("")
+            editRatingBar.rating = 0.0f
+            editFloatMenu.close(true)
+
+        }
+    }
+
+}
